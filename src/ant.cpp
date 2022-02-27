bc02189ac79535b7552b3d20578fd6d1af0a8419
Chris Carr <ccarr895@gmail.com>
2022-02-27 15:01:39 +0000

    formatting & performance improvements

--- src/ant.cpp
+++ src/ant.cpp
@@ -59,7 +59,7 @@ namespace full_coverage_path_planner
         std::list<Pos_proba> probabiltyDistro;
         for (Pos_phera pp : pheromonesList)
         {
-            _Float64 probability = pp.phera / pheramoneSum; //probability normalization
+            _Float64 probability = pp.phera / pheramoneSum; // probability normalization
             probabiltyDistro.push_back({pp.pos, probability});
         }
 
@@ -69,10 +69,10 @@ namespace full_coverage_path_planner
     void Ant::calculateProbabilityMatrix(std::vector<std::vector<_Float64>> global_pheromone_grid)
     {
         int i = 0;
-        for(std::vector<_Float64> y : personal_probability_matrix)
+        for (std::vector<_Float64> y : personal_probability_matrix)
         {
             int j = 0;
-            for(_Float64 x : y)
+            for (_Float64 x : y)
             {
                 personal_probability_matrix[i][j] = personal_pheromone_grid[i][j] / global_pheromone_grid[i][j];
                 j++;
@@ -97,11 +97,11 @@ namespace full_coverage_path_planner
 
             if (i == 0)
             {
-                xt = orig_x +1;
+                xt = orig_x + 1;
             }
             if (i == 1)
             {
-                xt = orig_x -1;
+                xt = orig_x - 1;
             }
             if (i == 2)
             {
@@ -112,31 +112,32 @@ namespace full_coverage_path_planner
                 yt = orig_y - 1;
             }
 
-            if (visited[yt][xt] == eNodeOpen)
+            if (xt >= 0 && xt < nCols && yt >= 0 && yt < nRows) // ant within bounds
             {
-                gridNode_t possible_movement = {{xt, yt}, 0, 0};
-                possible_movements.push_back(possible_movement);
+                if (visited[yt][xt] == eNodeOpen) // and location is not part of working memory
+                {
+                    gridNode_t possible_movement = {{xt, yt}, 0, 0}; // it's possible to move to that location
+                    possible_movements.push_back(possible_movement);
+                }
             }
-
             i++;
         }
 
         return possible_movements;
     }
 
-    bool Ant::resolveDeadlock(std::vector<std::vector<bool>> visited, std::list<Point_t> goals, std::vector<std::vector<bool>> const& grid)
+    bool Ant::resolveDeadlock(std::vector<std::vector<bool>> visited, std::list<Point_t> goals, std::vector<std::vector<bool>> const &grid)
     {
         std::list<gridNode_t> pathNodesForAstar = current_path;
 
         bool resign = a_star_to_open_space(grid, current_location, 1, visited, goals, pathNodesForAstar);
 
-
-        if(!resign)
+        if (!resign)
         {
             current_path = pathNodesForAstar;
         }
 
-        //update visited grid from a*
+        // update visited grid from a*
         std::list<gridNode_t>::iterator it;
         for (it = current_path.begin(); it != current_path.end(); ++it)
         {
@@ -171,15 +172,15 @@ namespace full_coverage_path_planner
         int nx = new_location.pos.x;
         int ny = new_location.pos.y;
 
-        if (cx+1 == nx)
+        if (cx + 1 == nx)
         {
             direction = EAST;
         }
-        else if (cx-1 == nx)
+        else if (cx - 1 == nx)
         {
             direction = WEST;
         }
-        else if (cy+1 == ny)
+        else if (cy + 1 == ny)
         {
             direction = NORTH;
         }
@@ -188,8 +189,7 @@ namespace full_coverage_path_planner
             direction = SOUTH;
         }
 
-
-        current_location = new_location; //gridNode_t
+        current_location = new_location; // gridNode_t
         if (add_to_path)
         {
             current_path.push_back(current_location);
